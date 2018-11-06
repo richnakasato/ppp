c75f5730576ba835c924d16c1591ffef31cc80e6
Rich Nakasato <richnakasato@hotmail.com>
2018-11-05 23:31:16 -0500

    working through chapter 5

---
+++ ch5/syntax_errors.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int area(int x) {
+    return x;
+}
+
+int main() {
+    int s1 = area(7;
+    int s2 = area(7)
+    Int s3 = area(7);
+    int s4 = area('7);
+
+    return 0;
+}
